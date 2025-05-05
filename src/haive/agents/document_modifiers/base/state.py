e357b320f5fb7550434ce51f42d2377f2cade317
0rac130fD31phi <william.astley@algebraicwealth.com>
2025-05-05 17:27:26 -0400

    started base, state

---
+++ src/haive/agents/document_modifiers/base/state.py
@@ -0,0 +1,59 @@
+
+from typing import List, Optional
+from haive.core.schema import StateSchema
+from pydantic import BaseModel, Field, computed_field,\
+    model_validator,field_validator
+from langchain_core.documents import Document
+
+class DocumentModifierState(StateSchema):
+    """
+    State for the document modifier agent.
+    """
+    #pass
+    name: Optional[str] = Field(default=None,description="The name of the document modifier.")
+    description: Optional[str] = Field(default=None,description="The description of the document modifier.")
+    documents:List[Document] = Field(default_factory=list,description="The documents to process.")
+    #is_split: bool = Field(default=False,description="Whether the documents are split.")
+    embeded_documents:List[Document] = Field(default_factory=list,description="The embeded documents.")
+    split_documents:List[Document] = Field(default_factory=list,description="The split documents.")
+    transformed_documents:List[Document] = Field(default_factory=list,description="The transformed documents.")
+    #annotations:List[Annotation]
+    @classmethod
+    def from_documents(cls,documents:List[Document]) -> "DocumentModifierState":
+        """
+        Create a DocumentModifierState from a list of documents.
+        """
+        return cls(documents=documents)
+    
+    @computed_field
+    @property
+    def documents_text(self) -> str:
+        """
+        The text of the documents.
+        """
+        return "\n".join([doc.page_content for doc in self.documents])
+    @computed_field
+    @property
+    def num_documents(self) -> int:
+        """
+        The number of documents.
+        """
+        return len(self.documents)
+    
+    @model_validator(mode="after")
+    def validate_documents(self):
+        """
+        Validate the documents.
+        """
+        if self.num_documents == 0:
+            raise ValueError("At least one document is required.")
+        return self
+    
+    @field_validator("documents")
+    def validate_documents(cls,v):
+        """
+        Validate the documents.
+        """
+        return v
+    
+    
